e97323908252a84550f14b6b631ff6c6cf324f1c
nadobrranichh <nazarshch26@gmail.com>
2026-04-17 12:43:14 +0300

    feat: added a ui for the landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,3 +1,12 @@
+import Button from "../components/Button";
+
 export default function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <main className="flex-1 bg-main text-secondary flex flex-col items-center justify-center gap-3">
+      <h2 className="text-5xl font-bold">Scribbble</h2>
+      <Button className="px-4 py-2">Join a room</Button>
+
+      <Button className="px-4 py-2">Create a room</Button>
+    </main>
+  );
 }
