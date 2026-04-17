8a9d2a1a7a50f10f2936f04e20d40da42cdbf12f
nadobrranichh <nazarshch26@gmail.com>
2026-04-17 14:32:42 +0300

    feat: added joinpage to look for rooms and join them

---
+++ src/components/Room.tsx
@@ -0,0 +1,8 @@
+export default function Room() {
+  return (
+    <div className="w-2/5 border-2 rounded-xl border-secondary text-secondary text-center pb-8">
+      <p className="text-xl">Room ABCD-1234-WXYZ</p>
+      <p>X users drawing</p>
+    </div>
+  );
+}
--- src/pages/JoinPage.tsx
+++ src/pages/JoinPage.tsx
@@ -1,3 +1,21 @@
+import Button from "../components/Button";
+import SearchIcon from "../assets/search-icon.svg";
+
 export default function JoinPage() {
-  return <div>JoinPage</div>;
+  return (
+    <main className="flex-1 flex flex-col items-center bg-main gap-3">
+      <div className="py-10">
+        <p className="text-secondary text-center text-xl">Enter Room ID:</p>
+        <input
+          name="room-id"
+          type="text"
+          className="border-2 border-secondary rounded-md text-xl h-10 align-top text-center w-60 text-secondary"
+          maxLength={14}
+        />
+        <Button className="ml-1 p-3">
+          <img src={SearchIcon} className="w-4" />
+        </Button>
+      </div>
+    </main>
+  );
 }
