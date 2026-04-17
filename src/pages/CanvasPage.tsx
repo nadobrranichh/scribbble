16750daf14df14edf529144dfa1a8ec1c4d12635
nadobrranichh <nazarshch26@gmail.com>
2026-04-17 14:35:09 +0300

    fix: fixed canvas page to adapt its size to the viewport

--- src/pages/CanvasPage.tsx
+++ src/pages/CanvasPage.tsx
@@ -2,7 +2,7 @@ import CanvasSection from "../components/CanvasSection";
 
 export default function CanvasPage() {
   return (
-    <main className="h-full w-full flex relative">
+    <main className="flex-1 flex relative">
       {/* toolbar */}
       <section className="toolbar bg-secondary h-full w-20"></section>
       {/* main canvas area */}
