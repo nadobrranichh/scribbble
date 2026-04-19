cb941b30ead42d3377c0701840d8a218b925f167
nadobrranichh <nazarshch26@gmail.com>
2026-04-19 13:57:37 +0300

    feat: updated room component to send out events on joining

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,8 +1,17 @@
-export default function Room() {
+import { socket } from "../socket/socket";
+
+export default function Room({ id, users }: { id: string; users: number }) {
+  function handleClick() {
+    socket.emit("join-room", id);
+  }
+
   return (
-    <div className="w-2/5 border-2 rounded-xl border-secondary text-secondary text-center pb-8">
-      <p className="text-xl">Room ABCD-1234-WXYZ</p>
-      <p>X users drawing</p>
+    <div
+      className="w-2/5 border-2 rounded-xl border-secondary text-secondary bg-main text-center pb-8 cursor-pointer hover:brightness-95"
+      onClick={handleClick}
+    >
+      <p className="text-xl">Room {id}</p>
+      <p>{users} users drawing</p>
     </div>
   );
 }
