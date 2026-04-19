bcabfb41b01983defcc9b498c43e2d2e892c69e2
nadobrranichh <nazarshch26@gmail.com>
2026-04-19 12:07:27 +0300

    feat: added active room id display to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import { useRoomIdStore } from "../store/room-id-store";
+
 export default function Header() {
+  const { roomId } = useRoomIdStore();
   return (
-    <header className="h-20 bg-secondary px-8 flex items-center ">
+    <header className="h-20 bg-secondary px-8 flex items-center justify-between ">
       <p className="text-main text-3xl">Scribbble</p>
+      {roomId && <p className="text-main">Room ID: {roomId}</p>}
     </header>
   );
 }
