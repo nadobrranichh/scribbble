860ce99d0db89c467079420896040533145a7212
nadobrranichh <nazarshch26@gmail.com>
2026-04-19 14:02:36 +0300

    feat: added real-time drawing events

--- src/components/CanvasSection.tsx
+++ src/components/CanvasSection.tsx
@@ -1,16 +1,34 @@
 import { useEffect, useRef } from "react";
 import { colorsList, widthsList } from "../lists/canvas";
+import { useLocation } from "react-router";
+import type { Location } from "react-router";
+import type { Point, RoomType, Stroke } from "../types/types";
+import { socket } from "../socket/socket";
+import { useRoomIdStore } from "../store/room-id-store";
 
 export default function CanvasSection() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const ctxRef = useRef<CanvasRenderingContext2D>(null);
   const isDrawing = useRef<boolean>(false);
-  const lastPosition = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
+  const lastPosition = useRef<Point>({ x: 0, y: 0 });
+  const { roomId } = useRoomIdStore();
+  const location = useLocation() as Location<RoomType>;
 
   function setCtxColor(color: string) {
     if (!ctxRef.current) return;
     ctxRef.current.strokeStyle = color;
     console.log(color, ctxRef.current.strokeStyle);
+  }
+
+  function draw(from: Point, to: Point, color?: string, lineWidth?: number) {
+    const ctx = ctxRef.current;
+    if (!ctx) return;
+    if (color) ctx.strokeStyle = color;
+    if (lineWidth) ctx.lineWidth = lineWidth;
+    ctx.beginPath();
+    ctx.moveTo(from.x, from.y);
+    ctx.lineTo(to.x, to.y);
+    ctx.stroke();
   }
 
   function handleMouseDown(e: React.MouseEvent<HTMLCanvasElement>) {
@@ -24,14 +42,22 @@ export default function CanvasSection() {
   function handleMouseMove(e: React.MouseEvent<HTMLCanvasElement>) {
     const ctx = ctxRef.current;
     if (!isDrawing.current || !ctx) return;
-    ctx.beginPath();
-    ctx.moveTo(lastPosition.current.x, lastPosition.current.y);
-    ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
-    ctx.stroke();
-    lastPosition.current = {
+    const newPosition = {
       x: e.nativeEvent.offsetX,
       y: e.nativeEvent.offsetY,
-    };
+    } as Point;
+
+    draw(lastPosition.current, newPosition);
+    socket.emit("draw", {
+      stroke: {
+        points: [lastPosition.current, newPosition],
+        color: typeof ctx.strokeStyle === "string" ? ctx.strokeStyle : "#000",
+        lineWidth: ctx.lineWidth.toString(),
+        userId: socket.id || "",
+      } as Stroke,
+      roomId,
+    });
+    lastPosition.current = newPosition;
   }
 
   useEffect(() => {
@@ -50,6 +76,22 @@ export default function CanvasSection() {
     ctx.lineWidth = 2.5;
     ctx.lineCap = "round";
     ctx.lineJoin = "round";
+
+    //reacting to other users' drawings
+    socket.on("drawn", (data: { stroke: Stroke }) => {
+      const {
+        stroke: { points, color, lineWidth },
+      } = data;
+      draw(points[0], points[1], color, Number.parseInt(lineWidth));
+    });
+  }, []);
+
+  useEffect(() => {
+    const room = location.state;
+    if (!room) return;
+    room.strokes.forEach(({ points, color, lineWidth }) => {
+      draw(points[0], points[1], color, Number.parseInt(lineWidth));
+    });
   }, []);
 
   return (
