5b98b5320b09f0767c6114e75a7e07ff08abe983
nadobrranichh <nazarshch26@gmail.com>
2026-05-27 13:54:29 +0300

    feat: added leaving to landing page from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,26 @@
+import ArrowLeftImg from "../assets/arrow-left.svg";
+import { useLocation, useNavigate } from "react-router";
 import { useRoomIdStore } from "../store/room-id-store";
 
 export default function Header() {
-  const { roomId } = useRoomIdStore();
+  const { roomId, clearRoomId } = useRoomIdStore();
+  const location = useLocation();
+  const navigate = useNavigate();
   return (
     <header className="h-20 bg-secondary px-8 flex items-center justify-between ">
-      <p className="text-main text-3xl">Scribbble</p>
+      <p className="text-main text-3xl flex items-center gap-5">
+        {location.pathname !== "/" && (
+          <button
+            onClick={() => {
+              navigate("/");
+              clearRoomId();
+            }}
+          >
+            <img className="w-8 cursor-pointer" src={ArrowLeftImg} />
+          </button>
+        )}
+        Scribbble
+      </p>
       {roomId && <p className="text-main">Room ID: {roomId}</p>}
     </header>
   );
