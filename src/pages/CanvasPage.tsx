5984268ca8a06610d9e9709dfeb22380f69488a1
nadobrranichh <nazarshch26@gmail.com>
2026-05-27 12:56:03 +0300

    chore: removed unused toolbar

--- src/pages/CanvasPage.tsx
+++ src/pages/CanvasPage.tsx
@@ -23,9 +23,6 @@ export default function CanvasPage() {
 
   return (
     <main className="flex-1 flex relative">
-      {/* toolbar */}
-      <section className="toolbar bg-secondary h-full w-20"></section>
-      {/* main canvas area */}
       <CanvasSection />
     </main>
   );
